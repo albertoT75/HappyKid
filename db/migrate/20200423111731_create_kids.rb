6cfa62d526d142ef86eaa34ccf94fa8d0ffec20f
Alberto Tarantino <albertotarantino75@yahoo.it>
2020-04-23 13:19:02 +0200

    creating models and controllers

--- db/migrate/20200423111731_create_kids.rb
+++ db/migrate/20200423111731_create_kids.rb
@@ -1,13 +1,8 @@
 class CreateKids < ActiveRecord::Migration[5.2]
   def change
     create_table :kids do |t|
-      t.string :first_name
-      t.references :user, foreign_key: true
 
       t.timestamps
     end
   end
 end
-
-
-
--- db/migrate/20200423111748_create_rewards.rb
+++ db/migrate/20200423111748_create_rewards.rb
@@ -1,9 +1,6 @@
 class CreateRewards < ActiveRecord::Migration[5.2]
   def change
     create_table :rewards do |t|
-      t.string :prize
-      t.integer :stars
-      t.references :game, foreign_key: true
 
       t.timestamps
     end
