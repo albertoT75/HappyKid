63de269ef6ed9bbace046da1a3b69c125cc5db1c
Alberto Tarantino <albertotarantino75@yahoo.it>
2020-04-27 16:01:34 +0200

    fixing query

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   resources :kids
-  resources :games, only: [:index, :show]
+  resources :games, only: [:index, :show] do
   # resources :games, only: [:index, :show] do
   #   resources :rewards only: [:new, :create]
-  # end
+  end
 
   get 'dashboard', to: 'pages#dashboard', as: :dashboard
 end
