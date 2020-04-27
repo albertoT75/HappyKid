b8c2bb1c9b04cec19327b157ffef700e6ee31136
Alberto Tarantino <albertotarantino75@yahoo.it>
2020-04-27 14:50:28 +0200

    implementing search

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,8 +1,74 @@
 class GamesController < ApplicationController
 
+  skip_before_action :authenticate_user!, only: [:index, :show]
+  before_action :set_game, only: [:show]
+
   def index
     @games = policy_scope(Game)
-    @games = Game.all
+    if params[:query].present?
+
+      @games = @games.search_by_name(params[:query])
+      if @games.empty?
+        flash[:alert] = "Sorry no area matches your search."
+        redirect_to(root_path)
+      else
+        @games = filter_results(@games)
+
+        session[:search_results] = request.url
+          if @games.empty?
+            flash[:alert] = "Sorry no area matches your search."
+            redirect_to(root_path)
+          end
+      end
+    elsif params[:query].blank?
+      @games = filter_results(@games)
+
+      session[:search_results] = request.url
+    else
+      flash[:alert] = "Sorry no area matches your search."
+      redirect_to(root_path)
+    end
   end
 
+  def show
+    if current_user
+    #   @all_reservations = current_user.reservations.where(kindergarden_id: @kindergarden.id)
+    #   @my_reservation = current_user.reservations.where(kindergarden_id: @kindergarden.id, status: 'Accepted')
+    #   @declined_reservations = current_user.reservations.where(kindergarden_id: @kindergarden.id, status: 'Declined')
+    #   @favourite = current_user.favourites.where(kindergarden_id: @kindergarden.id).first
+    #   @review = Review.new
+    #   @kids_with_no_reservation = current_user.kids.reject { |kid| Reservation.exists?(kid_id: kid.id, kindergarden_id: @kindergarden.id) }
+    #   # @reservation = Reservation.where(kid_id: current_user.kids. ?, kindergarden_id: @kindergarden.id)
+    # end
+    @kid = Kid.new
+    # @reservation = Reservation.new
+
+  end
+
+  def filter_results(games)
+    if params[:name].present?
+      @games = games.where(name: params[:name])
+    end
+
+    if params[:description].present?
+      @games = games.where(description: params[:description])
+    end
+
+    @games
+  end
+
+
+  private
+
+
+
+  def game_params
+    params.require(:game).permit(:name, :description)
+  end
+
+  def set_game
+    @game = Game.find(params[:id])
+    authorize @game
+  end
 end
+
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,5 +5,10 @@ Game.destroy_all
 
 games = Game.create([{name: "Sleeping - With mom and dad", description: "Help your children to find a balanced sleep, by staying in his bed during the night."},
                       {name: "Sleeping - Falling asleep alone", description: "Develop routines to make your children fall asleep alone."},
-                      {name: "Sleeping - Wondering at night", description: "Prevent your children from getting up when shortly awake."),
-                      {name: "Potty Training", description: "Let's keep track of the bed-accidents!"}])
+                      {name: "Sleeping - Wondering at night", description: "Prevent your children from getting up when shortly awake."},
+                      {name: "Potty Training", description: "Let's keep track of the bed-accidents!"},
+                      {name: "Eating - Eating Alone", description: "Let's help our children to be able to eat alone."},
+                      {name: "Eating - Eating At The Table", description: "For kids who are used to get up from the table before being done with food."},
+                      {name: "Dressing Up - Choosing Clothes", description: "Let's get the children used to chose their own clothes."},
+                      {name: "Dressing Up - Wearing clothes", description: "Children dressing up without the help of the parents."},
+                      {name: "Home work", description: "Children doing their homework without the help of the parents."}])
